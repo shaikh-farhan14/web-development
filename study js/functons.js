62c32f33828b22f176251c2354ab3cebd68ec286
ajay <ajay1420er@gmail.com>
2025-05-22 14:40:33 +0530

    code

---
+++ study js/functons.js
@@ -0,0 +1,60 @@
+/* functions */
+
+// function farhan() {
+//     console.log(2+4)
+// }
+
+// farhan()
+
+
+// function fffff() {
+//     return 2-4
+// }
+
+// let ans = fffff();
+// console.log("ans", ans)
+
+
+
+// let num1=5;
+// let num2=5;
+// let num3=5;
+
+// function shaikh(a,b,c) {
+//     return a*b*c;
+// }
+
+//  let ans2 = shaikh(num1,num2,num3);
+//  console.log("ans2",ans2);
+
+// /* demo */
+
+// let food = "biryani";
+// khushi(food);
+
+// function khushi(value){
+//     hetuk(value)
+// }
+
+// function hetuk(value){
+//     nishad(value)
+// }
+
+// function nishad(value){
+//     farhan(value)
+// }
+
+// function farhan(value){
+//     console.log("value from farhan", value)
+// }
+
+/* Arrow function*/
+
+
+let faarhaan = () =>{
+console.log(5+5)
+}
+faarhaan();
+
+
+
--- study js/loops.js
+++ study js/loops.js
@@ -32,14 +32,100 @@
 //     console.log(pattern);
 // }
 
-const n= 7;
-let a = 0, b = 3;
+// const n= 7;
+// let a = 0, b = 3;
 
-for (let i = 0; i <= n; i++) {
-    console.log(a);
-    let next = a + b;
-    a = b;
-    b = next;
-}
+// for (let i = 0; i <= n; i++) {
+//     console.log(a);
+//     let next = a + b;
+//     a = b;
+//     b = next;
+// }
 
 
+/* while loop */
+
+// let num = 9;
+// let i = 0;
+// let sum = null;
+// while (i < num) {
+//     sum += i;
+//     i++;
+//     console.log("sum", sum);
+// }
+
+
+/* task */
+
+// let start=1;
+// let end=100;
+
+// while(start<=end){
+//     console.log(start);
+//     start++;
+// }
+
+// let num=2;
+// let i=0;
+// let sum=10;
+// while(i<=num){
+//     sum+=i;
+//     i++;
+//     console.log("sum",sum);
+// }
+
+
+// let n = parseInt(process.argv[2]);
+// let i1 =2;
+// while(i1<=n) {
+//     if(i1 % 2 ==0){
+//         console.log("this is even number", i)
+// }
+// i1++;
+// }
+
+
+// let n = parseInt(process.argv[2]);
+// let i1 =2;
+// while(i1<=n) {
+//     if(i1 % 2 ==0){
+//         console.log("this is odd number", i1)
+// }
+// i1++;
+// }
+
+
+/* guess the number*/
+
+// let guessnumber = Math.floor(match.random() *10) +1;
+
+// let usernumber = parseInt(prompt.apply("enter the number"));
+
+// while(guessnumber!== usernumber){
+//     usernumber = parseInt(prompt("enter the number"));
+
+// }
+// console.log("this is your guessed number", guessnumber)
+
+
+// let str = process.argv[2];
+// let length = str.length -1;
+// let index = 0;
+// let reverStr ="";
+
+// while (length >= index) {
+//     reverStr += str[length];
+//     length--;
+// }
+
+// console.log("reverStr",reverStr)
+
+
+/*  do while loop */
+
+let number = 7;
+
+do{
+    console.log(number);
+    number++;
+}while(number <6 );
