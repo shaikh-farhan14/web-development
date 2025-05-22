73ca1a181642aa30fbf9478a7a6f6cae953ec3a5
ajay <ajay1420er@gmail.com>
2025-05-22 15:42:12 +0530

    hello

--- study js/calculator.js
+++ study js/calculator.js
@@ -1,1 +1,35 @@
 
+/* Calculator */
+
+
+let num1 = 2
+let num2 = 3
+
+function add(num1,num2){
+    return num1+num2;
+}
+console.log(add(num1,num2));
+
+function sub(num1,num2){
+    return num1-num2;
+}
+console.log(sub(num1,num2))
+
+function mul(num1,num2){
+    return num1*num2;
+}
+console.log(mul(num1,num2))
+
+function div(num1,num2){
+    return num1/num2;
+}       
+console.log(div(num1,num2))
+
+
+
+
+
+
+
+    
+    
