19c3666766b5a4269b2b3fa05923a4a7028c1c81
ajay <ajay1420er@gmail.com>
2025-06-10 16:15:54 +0530

    object

---
+++ study js/object.js
@@ -0,0 +1,95 @@
+
+// This is object
+
+// Template literal 
+
+// Object literal
+
+// let car ={
+//     brand:"Mustang",
+//     model:"dark horse",
+//     years:"2025",
+//     automation:{
+//         name:"Mustang",
+//         model:"Gt",
+//         years:"2023",
+//     },
+//     myfunc: function(){
+//     console.log("hello")
+//     },
+//     hobbies:["valorant,Bgmi,cod"],
+//     married:false,
+// }
+
+// dot operator
+
+// for (let key in car) {
+//     console.log("key", key);
+//     console.log("value", car[key]);
+// }
+
+// car.years ="2024"
+// car.city="mumbai"
+// console.log("car", car);
+
+
+
+// task
+
+
+// let data={
+//     name:"farhan",
+//     age:"19",
+//     gender:"male",
+
+//     myfunc: function(){
+//     console.log("hello,my name is farhan")
+//     },
+// }
+
+
+// for (let key in data) {
+//     console.log("key", key);
+//     console.log("value", data[key]);
+// }
+// data.myfunc()
+
+
+// let student={
+//     name:"fern",
+//     address:{
+//         city:"mumbai",
+//         state:"maharashtra",
+//         pincode:400065,
+//     },
+//     grades:{
+//         math:90,
+//         science:80,
+//         social:70,
+//     }
+// }
+
+// for (let object in student) {
+//     console.log("object", object);
+//     console.log("value", student[object]);
+// }
+
+
+let fruits={
+    name:"apple",
+    color:"red",
+    price:100,
+    quantity:5,
+    Option2:{
+        name:"Mango",
+        color:"yellow",
+        price:150,
+        quantity:7,
+    }
+}
+for (let object in fruits) {
+    console.log("object", object);
+    console.log("value", fruits[object]);
+}
+
+
