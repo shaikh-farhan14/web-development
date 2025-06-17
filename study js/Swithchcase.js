fd49a22b3c02aebdacd1bff34f18d671334ef760
ajay <ajay1420er@gmail.com>
2025-06-17 16:33:58 +0530

    array

--- study js/Swithchcase.js
+++ study js/Swithchcase.js
@@ -36,50 +36,54 @@ switch (key) {
 // }      
 
 
-// let grade = "E";
+let grade = "B";
 
-// switch (grade) {
-//     case 'A':
-//         console.log("Excellent")
-//     case 'B':
-//         console.log("Good")
-//     case 'C':
-//         console.log("Average")
-//     case 'D':
-//         console.log("Fail")
-//     case 'E':
-//         console.log("Fail")
-//         break;
-//     case 'F':
-//         console.log("Fail")
-//     default:
-//         console.log("Fail")
-// }
-
-let month = 11;
-
-switch (month) {
-
-    case 12:
-    case 1:
-    case 2:
-        console.log("Winter")
+switch (grade) {
+    case 'A':
+        console.log("Excellent")
         break;
-    case 3:
-    case 4:
-    case 5:
-        console.log("Spring")
+    case 'B':
+        console.log("Good")
         break;
-    case 6:
-    case 7:
-    case 8:
-        console.log("Summer")
+    case 'C':
+        console.log("Average")
         break;
-    case 9:
-    case 10:
-    case 11:
-        console.log("Autumn")
+    case 'D':
+        console.log("Fail")
+        break;
+    case 'E':
+        console.log("Fail")
+        break;
+    case 'F':
+        console.log("Fail")
+    default:
+        console.log("Fail")
 }
+
+// let month = 11;
+
+// switch (month) {
+
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log("Winter")
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("Spring")
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log("Summer")
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log("Autumn")
+// }
 
 
 
---
+++ study js/array.js
@@ -0,0 +1,39 @@
+
+let arr1 =[
+    { name :'mehfooz', age :88 },
+    {name :'khushi', age :200 },
+];
+
+let arr2 =[
+    false,
+    true,
+    false,
+]
+
+let arr3=[1,2,3,[4,5,[6,7,8],[9,10]]];
+// console.log('arr3',arr3);
+
+
+let arr = ['mehfooz','farhan','hetuk','khushi'];
+
+// array.push(11,12)
+array.pop();
+
+console.log('array',array);
+
+let arr4=[
+    [1,2,3],
+    [1,2,3],
+    [1,2,3],
+]
+
+// console.log('arr4',arr4);
+
+// for (let i = 1; i < array.length; i++) {
+//         console.log('i',i);
+//     }
+
+
+// array.foreach[(element)=>{
+//     console.log('element',element);
+// }];
