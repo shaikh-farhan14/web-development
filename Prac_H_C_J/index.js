55ea80bd7dbd363baa43b7f59cb821713858dc1f
shaikh-farhan14 <shaikhfarhan1405@gmail.com>
2025-07-30 16:52:35 +0530

    project

--- Prac_H_C_J/index.js
+++ Prac_H_C_J/index.js
@@ -1,12 +1,33 @@
 let Api = `https://fakestoreapi.com/products`
 
+let storage = JSON.parse(localStorage.getItem('token'));
+
+let cartArr = JSON.parse(localStorage.getItem('cartItem')) || [];
+console.log('🚀 ~ cartArr:', cartArr.length < 0);
+
+const path = window.location.pathname;
+console.log('🚀 ~ path:', path);
+
+const cartLength = document.querySelector('span');
+
+
+if (path === '/Project/FakeStore/index.html') {
+    cartLength.style.display = cartArr.length < 0 === false ? 'none' : 'block';
+    cartLength.className = cartArr.length > 0 ? 'cartLength-active' : 'none';
+    cartLength.innerText = cartArr.length > 0 ? cartArr.length : '';
+}
+
+
+
+// console.log('🚀 ~ storage:', storage);
+
 const datafetch = async () => {
     try {
         let res = await fetch(Api);
         let data = await res.json();
         storeUI(data);
     } catch (error) {
-      
+        // console.log('🚀 ~ error:', error);
     }
 }
 
@@ -14,7 +35,7 @@ const storeUI = (value) => {
     const dataInfo = document.querySelector("#dataInfo");
 
     value?.forEach((element) => {
-       
+        // console.log('🚀 ~ element:', element);
         const div = document.createElement('div');
         const pricingDiv = document.createElement('div');
         const id = document.createElement('p');
@@ -64,4 +85,76 @@ const storeUI = (value) => {
 
     });
 
+}
+
+
+//  =============== from js =====================
+
+
+
+const formSubmitData = async (e) => {
+    e.preventDefault();
+
+    const loginApi = `https://fakestoreapi.com/auth/login`;
+
+
+    const email = document.querySelector("#username").value;
+    // console.log('🚀 ~ email:', typeof email);
+    const pass = document.querySelector("#password").value;
+    // console.log('🚀 ~ pass:', typeof pass);
+
+    /* 
+    johnd -> username
+    m38rmF$ -> pass    
+    */
+
+    let loginData = {
+        username: email,
+        password: pass
+    }
+
+    try {
+        let res = await fetch(loginApi, {
+            method: 'POST',
+            body: JSON.stringify(loginData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        let data = await res.json();
+        // console.log('🚀 ~ data:', data);
+
+        localStorage.setItem('token', JSON.stringify(data.token));
+        if (storage) {
+            window.location = 'index.html';
+            localStorage.removeItem('token');
+        }
+    } catch (error) {
+        // console.log('🚀 ~ error:', error);
+    }
+
+}
+// https://fakestoreapi.com/carts
+// https://fakestoreapi.com/products
+
+//  =============== login invoke js =====================
+
+const changeToLogin = () => {
+    window.location = 'Login.html'
+}
+
+const backFun = () => {
+    window.location = 'index.html'
+}
+
+const chageToCart = () => {
+    window.location = 'Cart.html'
+}
+
+const cartDisplay = () => {
+    storeUI(cartArr)
+}
+
+const changeToCheckout = () => {
+    window.location = 'checkout.html'
 }
